695e5d5f9da884c88ed4d521151f44c418e2bbdd
Florian Lauch <florian.lauch@epitech.eu>
2023-03-20 14:48:17 +0100

    feat(bootstrap): closest

--- src/Math.hs
+++ src/Math.hs
@@ -12,3 +12,7 @@ module Math (
 distance :: (Int, Int, Int) -> (Int, Int, Int) -> Float
 distance (x1, y1, z1) (x2, y2, z2) =
     sqrt (fromIntegral ((x2 - x1) ^ 2 + (y2 - y1) ^ 2 + (z2 - z1) ^ 2))
+
+closest :: [(Int, Int, Int)] -> (Int, Int, Int) -> (Int, Int, Int)
+closest [] _ = (0, 0, 0)
+closest (x:xs) y = foldl (\acc x -> if distance x y < distance acc y then x else acc) x xs
