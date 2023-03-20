e196e7ec35af348afef7a8cfb0c6fb8031464b11
Florian Lauch <florian.lauch@epitech.eu>
2023-03-20 12:05:36 +0100

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+{-
+-- EPITECH PROJECT, 2022
+-- ImageCompressor
+-- File description:
+-- Setup
+-}
+
+module Main (main) where
+
+import Lib
+
+main :: IO ()
+main = imageCompressor
---
+++ src/Lib.hs
@@ -0,0 +1,13 @@
+{-
+-- EPITECH PROJECT, 2022
+-- ImageCompressor
+-- File description:
+-- Core
+-}
+
+module Lib (
+    imageCompressor
+) where
+
+imageCompressor :: IO ()
+imageCompressor = putStrLn "imageCompressor"
