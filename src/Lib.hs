52e4b5d99952fdd5c3eec281bd22cd0f3f52f9e6
Florian Lauch <florian.lauch@epitech.eu>
2023-03-24 16:33:21 +0100

    feat(args): arguments handling using optparse-applicative

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,5 +9,23 @@ module Lib (
     imageCompressor
 ) where
 
+import Args
+import Options.Applicative
+import System.Exit
+
 imageCompressor :: IO ()
-imageCompressor = putStrLn "imageCompressor"
+imageCompressor = checkArguments =<< execParser opts
+    where opts = (info (getSample <**> helper)
+            ( fullDesc
+            <> progDesc "Compress an image"
+            <> header "ImageCompressor" )) { infoFailureCode = 84 }
+
+checkArguments :: Sample -> IO ()
+checkArguments (Sample c l _) | c < 1 =
+    errorMessage "Invalid number of colors"
+                              | l <= 0 =
+    errorMessage "Invalid convergence limit"
+                              | otherwise = putStrLn "GOOD JOB"
+
+errorMessage :: String -> IO ()
+errorMessage s = putStrLn s >> exitWith (ExitFailure 84)
