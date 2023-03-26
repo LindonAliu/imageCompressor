3e2531884da34f2517eae42dcb59cb7bf0e6b857
Lindon ALIU <lindon.aliu@epitech.eu>
2023-03-26 15:31:20 +0200

    feat: add main test for getRandomColorInList

--- src/Math.hs
+++ src/Math.hs
@@ -8,6 +8,8 @@
 module Math
     ( closest
     , distance
+    , getRandomColorInList
+    , randomInt
     ) where
 
 import Lib
@@ -24,9 +26,9 @@ closest [] _ = (0, 0, 0)
 closest (x:xs) y =
     foldl (\acc f -> if distance f y < distance acc y then f else acc) x xs
 
-getRandomColorInList :: [Color] -> Color
-getRandomColorInList [] = (0, 0, 0)
-getRandomColorInList list = list !! (randomInt (0, length list - 1) (mkStdGen 42))
+getRandomColorInList :: [Color] -> StdGen -> Color
+getRandomColorInList [] _ = (0, 0, 0)
+getRandomColorInList list gen = list !! (randomInt (0, (length list) - 1) gen)
 
 randomInt :: (Int, Int) -> StdGen -> Int
 randomInt range gen = head $ randomRs range gen
